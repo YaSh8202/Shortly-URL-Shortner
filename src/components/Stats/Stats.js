e45a6e15d2706c0bc522716803744d6427b73bdd
Yash <bajajy82@gmail.com>
2022-06-24 21:51:06 +0530

    Responsiveness added to Stat.js component

--- src/components/Stats/Stats.js
+++ src/components/Stats/Stats.js
@@ -8,8 +8,8 @@ import fullyCustomizable from "../../images/icon-fully-customizable.svg";
 const Stats = () => {
   return (
     <div className="lg:w-9/12 w-[85%] m-auto mt-28 flex flex-col mb-28 ">
-      <div className=" text-center lg:w-2/4 m-auto flex flex-col mb-24 ">
-        <h2 className="text-3xl font-bold mb-3 text-veryDarkBlue ">
+      <div className=" text-center lg:w-2/4 m-auto flex flex-col mb-24  ">
+        <h2 className="text-3xl font-bold mb-3 text-veryDarkBlue  ">
           Advanced Statistics
         </h2>
         <p className="text-lg text-grayishViolet ">
@@ -17,21 +17,21 @@ const Stats = () => {
           statistics dashboard
         </p>
       </div>
-      <div className="flex gap-7 relative ">
-        <div className="-z-10 left-12 top-36 absolute m-auto bg-cyan w-4/6 h-2" ></div>
+      <div className="flex gap-7 relative lg:flex-row flex-col ">
+        <div className="-z-10 lg:left-12 lg:top-36 absolute m-auto bg-cyan lg:w-4/6 lg:h-2 h-full w-2 left-[calc(50%-10px)] "></div>
         <Stat
           icon={brandRecognition}
           heading="Brand Recognition"
           text="Boost your brand recognition with each click. Generic links don't mean a thing. Branded links help instil confidence in your content."
         />
         <Stat
-        className="top-10"
+          className="top-10"
           icon={detailedRecords}
           heading="Detailed Records"
           text="Gain insights into who is clicking your links. Knowing when and where people engage with your content helps inform better decisions."
         />
         <Stat
-        className="top-20"
+          className="top-20"
           icon={fullyCustomizable}
           heading="Fully Customizable"
           text="Improve brand awareness and content discoverability through customizable links, supercharging audience engagement."
